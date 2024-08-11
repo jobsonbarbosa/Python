fac691f2808eb031f560250e0e77ca635e1cc91b
jobsonbarbosa <jobsonbarbosa86@gmail.com>
2024-08-10 23:46:51 -0300

    Manipulando string

---
+++ SABOR-EXPRESS/pratica/pratica-01.py
@@ -0,0 +1,18 @@
+# 1- Imprima a frase: Python na Escola de Programação da Alura.
+
+print('Python na Escola de Programação da Alura \n')
+
+# 2 - Imprima a frase: Meu nome é {nome} e tenho {idade} anos em que nome e idade precisam ser valores armazenados em variáveis.
+nome = 'Jobson'
+idade = 38
+
+print(f'Meu nome é {nome} e tenho {idade} anos. \n')
+
+#3 - Para imprimir a palavra ‘ALURA’ com cada letra em cada linha com a função print utilizando o parâmetro sep para especificar o separador entre os argumentos da seguinte forma:
+
+print('A', 'L', 'U', 'R', 'A', sep='\n')
+
+# 4 - Imprima a frase: O valor arredondado de pi é: {pi_arredondado} em que o valor de pi precisa ser armazenado em uma variável e arredondado para apenas duas casas decimais. Para facilitar, utilize:
+
+pi_arredondado = round(3,14159265358979323846)
+print('\n', pi_arredondado)
