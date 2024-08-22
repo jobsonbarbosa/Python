6917630149c37e5e23056f2c43663befef06a8e0
jobsonbarbosa <jobsonbarbosa86@gmail.com>
2024-08-22 20:15:53 -0300

    arquivos estaticos

--- PROJETO-SPACE/galeria/views.py
+++ PROJETO-SPACE/galeria/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('<h1> Jobs Space </h1><p>Bem vindo ao espaço</p>')
+    return render(request, 'index.html')
 
