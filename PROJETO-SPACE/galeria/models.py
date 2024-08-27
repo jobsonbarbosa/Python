0a0c5baa9eac9c1f3a5e1c4460be21c01bc0f461
jobsonbarbosa <jobsonbarbosa86@gmail.com>
2024-08-26 21:30:17 -0300

    Lidando com banco de dados, configurando o SQLite

--- PROJETO-SPACE/galeria/models.py
+++ PROJETO-SPACE/galeria/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Fotografia(models.Model):
+    nome = models.CharField(max_length=100, null=False, blank=False)
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False)
+    foto = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return f'Fotogradia [nome = {self.nome}]'
+
+'''
+para que o modelo funcione é necessário executar o comanando a seguir no terminal
+pyhton manage.py makemigrations
+
+'''
