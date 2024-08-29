339cca94eb51bb4d52f4f67b6308f6d76679d5ab
jobsonbarbosa <jobsonbarbosa86@gmail.com>
2024-08-28 22:07:57 -0300

    Listando jogos usando o Flask

--- FLASH/JOGOTECA/jogoteca.py
+++ FLASH/JOGOTECA/jogoteca.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
+
+class jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
 
 app = Flask(__name__)
 
 @app.route('/inicio')
 
 def ola():
-    return '<h1> Olá Jobson Barbosa</h1>'
+    jogo1 = jogo('Tetris', 'Puzzle', 'Atari')
+    jogo2 = jogo('God of War', 'Rack n Slash', 'PS2')
+    jogo3 = jogo('Mortal Kombat', 'Luta', 'PS2')
+    lista = [jogo1, jogo2, jogo3]
+    return render_template('lista.html', titulo = 'Jogos', jogos = lista)
 
 app.run()
