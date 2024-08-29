ad11329c70bfca8611780f395dbcf19010559a31
jobsonbarbosa <jobsonbarbosa86@gmail.com>
2024-08-29 20:59:13 -0300

    Criação de um novo jogo, criando formulário, Resolvendo um POST no servidor

--- FLASH/JOGOTECA/jogoteca.py
+++ FLASH/JOGOTECA/jogoteca.py
@@ -1,20 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
-class jogo:
+class Jogo:
     def __init__(self, nome, categoria, console):
         self.nome = nome
         self.categoria = categoria
         self.console = console
+        
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God of War', 'Rack n Slash', 'PS2')
+jogo3 = Jogo('Mortal Kombat', 'Luta', 'PS2')
+lista = [jogo1, jogo2, jogo3]
 
 app = Flask(__name__)
 
-@app.route('/inicio')
+@app.route('/')
 
-def ola():
-    jogo1 = jogo('Tetris', 'Puzzle', 'Atari')
-    jogo2 = jogo('God of War', 'Rack n Slash', 'PS2')
-    jogo3 = jogo('Mortal Kombat', 'Luta', 'PS2')
-    lista = [jogo1, jogo2, jogo3]
+def index():
     return render_template('lista.html', titulo = 'Jogos', jogos = lista)
+
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+
+@app.route('/criar', methods=['POST',])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
+    return render_template('lista.html', titulo='Jogos', jogos = lista)
 
 app.run()
